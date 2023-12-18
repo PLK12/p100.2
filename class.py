d984d07f87c04fba7a930014bb040756ffbe1841
PLK12 <88220947+PLK12@users.noreply.github.com>
2023-12-17 18:33:21 -0700

    Add files via upload

---
+++ class.py
@@ -0,0 +1,11 @@
+class ATM(object)
+    def __init__(number, password):
+        self.number = number
+        self.password = password
+    def withdraw(self):
+        print("withdrawn")
+    def password(self)
+        print("password required")
+
+myATM = ATM(111222333, 123)
+print(myATM.password)
